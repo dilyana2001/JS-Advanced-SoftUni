17443839f24a4f55428dca99aed3e4a576ef20ac
dilyana2001 <dilyana2001@gmail.com>
2021-07-15 18:59:50 +0300

    new Solutions

--- js-advanced/advanced-functions-exercise/08. Next Article/script.js
+++ js-advanced/advanced-functions-exercise/08. Next Article/script.js
@@ -1,3 +1,12 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let contentDiv = document.querySelector('#content');
+
+    function closure() {
+        let articleTag = document.createElement('article');
+        while (articles.length > 0) {
+            articleTag.textContent = articles.shift();
+            contentDiv.appendChild(articleTag);
+        } // sektor 8 avtogara => 350
+    }
+    return closure;
 }
