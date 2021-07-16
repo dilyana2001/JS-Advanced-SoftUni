6f4cbbfe250e0789a3588fc65822c6fc21590cad
dilyana2001 <dilyana2001@gmail.com>
2021-07-16 18:54:47 +0300

    cars solution

--- js-advanced/advanced-functions-lab/07-cars.js
+++ js-advanced/advanced-functions-lab/07-cars.js
@@ -1,26 +1,32 @@
-function cars() {
-    let iife = (function() {
-        return {
-            create(name) {
-                let obj = { name }
-            },
-            create(name, inherit, parentName) {
-                obj[name] = parentName
-            },
-            set(name, key, value) {
-                obj[name] = { key: value }
-            },
-            print(name) {
-                console.log(obj.name)
+function cars(arr) {
+    let cars = {};
+    let commandParser = {
+        create(carName, inherits, parentName) {
+            (cars[carName] = inherits ? Object.create(cars[parentName]) : {});
+        },
+        set(carName, key, value) {
+            cars[carName][key] = value;
+        },
+        print(carName) {
+            let output = [];
+            for (let element in cars[carName]) {
+                output.push(`${element}:${cars[carName][element]}`);
             }
-        }
-    })
+            console.log(output.join(', '));
+        },
+    }
+
+    arr.forEach(line => {
+        let [command, ...args] = line.split(' ');
+        commandParser[command](...args);
+    });
 }
-// cars([
-//     'create c1',
-//     'create c2 inherit c1',
-//     'set c1 color red',
-//     'set c2 model new',
-//     'print c1',
-//     'print c2'
-// ])
+
+cars([
+    'create c1',
+    'create c2 inherit c1',
+    'set c1 color red',
+    'set c2 model new',
+    'print c1',
+    'print c2'
+])
