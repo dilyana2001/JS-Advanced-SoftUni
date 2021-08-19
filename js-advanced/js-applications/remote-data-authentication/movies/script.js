433b4c2ecaafd02747327c4f393612e5cd6e7057
dilyana2001 <dilyana2001@gmail.com>
2021-08-19 16:49:22 +0300

    Furniture partition solution

--- js-advanced/js-applications/remote-data-authentication/movies/script.js
+++ js-advanced/js-applications/remote-data-authentication/movies/script.js
@@ -1,32 +1,30 @@
 function eventHandler() {
-    let movieUnList = document.getElementById('movieUnList')
-    let baseUrl = 'http://localhost:3030'
-    let registerForm = document.getElementById('register-form')
-    let movieForm = document.getElementById('add-movie-form')
-    let loginForm = document.getElementById('login-form')
+    let movieUnList = document.getElementById('movieUnList');
+    let baseUrl = 'http://localhost:3030';
+    let registerForm = document.getElementById('register-form');
+    let movieForm = document.getElementById('add-movie-form');
+    let loginForm = document.getElementById('login-form');
 
     fetch(`${baseUrl}/data/movies`)
         .then(res => res.json())
         .then(result => {
             Object.values(result).forEach(x => {
-                let movie = document.createElement('li')
-                let movieTitle = document.createElement('h3')
-                let movieDescription = document.createElement('p')
-                movieDescription.textContent = x.description
-                movieTitle.textContent = x.title
-                movie.appendChild(movieTitle)
-                movie.appendChild(movieDescription)
-                movieUnList.appendChild(movie)
-            })
+                let movie = document.createElement('li');
+                let movieTitle = document.createElement('h3');
+                let movieDescription = document.createElement('p');
+                movieDescription.textContent = x.description;
+                movieTitle.textContent = x.title;
+                movie.appendChild(movieTitle);
+                movie.appendChild(movieDescription);
+                movieUnList.appendChild(movie);
+            });
         })
+        .catch(() => console.log(`Error with loading movies`));
 
 
     movieForm.addEventListener('submit', (e) => {
-        e.preventDefault()
-        let formData = new FormData(e.currentTarget)
-
-        console.log([...formData.entries()])
-        console.log(formData.get('title'))
+        e.preventDefault();
+        let formData = new FormData(e.currentTarget);
 
         fetch(`${baseUrl}/data/movies`, {
                 method: 'POST',
@@ -43,16 +41,13 @@ function eventHandler() {
             .then(data => {
                 console.log(data)
             })
-            .catch(() => console.log(`error movieForm`))
-    })
+            .catch(() => console.log(`error movieForm`));
+    });
 
 
     loginForm.addEventListener('submit', (e) => {
-        e.preventDefault()
-        let formData = new FormData(e.currentTarget)
-
-        console.log([...formData.entries()])
-        console.log(formData.get('password'))
+        e.preventDefault();
+        let formData = new FormData(e.currentTarget);
 
         fetch(`${baseUrl}/users/login`, {
                 method: 'POST',
@@ -69,16 +64,13 @@ function eventHandler() {
                 saveToken(data.accessToken)
                 console.log(data);
             })
-            .catch(() => console.log(`error loginForm`))
+            .catch(() => console.log(`Error with login`));
     })
 
 
     registerForm.addEventListener('submit', (e) => {
-        e.preventDefault()
-        let formData = new FormData(e.currentTarget)
-
-        console.log([...formData.entries()])
-        console.log(formData.get('email'))
+        e.preventDefault();
+        let formData = new FormData(e.currentTarget);
 
         fetch(`${baseUrl}/users/register`, {
                 method: 'POST',
@@ -92,21 +84,19 @@ function eventHandler() {
             })
             .then(res => res.json())
             .then(data => {
-                console.log(data);
-                saveToken(data.accessToken)
-                registerForm.classList.add('hide') // setArtibute('class', 'hide)
-                    // movieForm.classList.remove('hide')
+                saveToken(data.accessToken);
+                registerForm.classList.add('hide');
             })
-            .catch(() => console.log(`error registerForm`))
-    })
+            .catch(() => console.log(`error registerForm`));
+    });
 
     function saveToken(token) {
-        localStorage.setItem('auth_token', token)
+        localStorage.setItem('auth_token', token);
     }
 
     function getToken() {
-        let token = localStorage.getItem('auth_token')
-        return token
+        let token = localStorage.getItem('auth_token');
+        return token;
     }
 }
-eventHandler()
+eventHandler();
