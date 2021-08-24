73b869bee474c306debcf9eed1172d8e76003654
dilyana2001 <dilyana2001@gmail.com>
2021-08-24 16:23:03 +0300

    calendar with notes update

--- js-advanced/js-applications/spa/lab/01.Calendar/app.js
+++ js-advanced/js-applications/spa/lab/01.Calendar/app.js
@@ -1,20 +1,20 @@
 function hideMonthsElements() {
-    const hiddenMonthElements = document.querySelectorAll('.monthCalendar');
-    for (const hiddenMonthElement of hiddenMonthElements) {
+    let hiddenMonthElements = document.querySelectorAll('.monthCalendar');
+    for (let hiddenMonthElement of hiddenMonthElements) {
         hiddenMonthElement.classList.add('hidden');
     }
 }
 hideMonthsElements();
 
 function hideDaysCalendarElements() {
-    const hiddenDaysCalendarElements = document.querySelectorAll('.daysCalendar');
-    for (const hiddenDaysCalendarElement of hiddenDaysCalendarElements) {
+    let hiddenDaysCalendarElements = document.querySelectorAll('.daysCalendar');
+    for (let hiddenDaysCalendarElement of hiddenDaysCalendarElements) {
         hiddenDaysCalendarElement.classList.add('hidden');
     }
 }
 hideDaysCalendarElements();
 
-const month = {
+let month = {
     Jan: 1,
     Feb: 2,
     Mar: 3,
@@ -34,8 +34,10 @@ document.querySelector('.yearsCalendar').addEventListener('click', (e) => {
     hideDaysCalendarElements();
     let currentYear = '';
     let currentMonth = '';
+    let currentDay = '';
+    let exactTdForAddingANote = '';
     if (e.target.tagName == 'DIV' || e.target.tagName == 'TD') {
-        const dataLink = e.target.querySelector('div') || e.target;
+        let dataLink = e.target.querySelector('div') || e.target;
         currentYear = dataLink.textContent;
         document.getElementById(`year-${currentYear}`).classList.remove('hidden');
 
@@ -44,9 +46,49 @@ document.querySelector('.yearsCalendar').addEventListener('click', (e) => {
     document.getElementById(`year-${currentYear}`).addEventListener('click', (e) => {
         hideDaysCalendarElements();
         if (e.target.tagName == 'DIV' || e.target.tagName == 'TD') {
-            const dataLink = e.target.querySelector('div') || e.target;
+            let dataLink = e.target.querySelector('div') || e.target;
             currentMonth = dataLink.textContent;
             document.getElementById(`month-${currentYear}-${month[currentMonth]}`).classList.remove('hidden');
         }
+
+        document.getElementById(`month-${currentYear}-${month[currentMonth]}`).addEventListener('click', (e) => {
+            if (e.target.tagName == 'DIV' || e.target.tagName == 'TD') {
+                let dataLink = e.target.querySelector('div') || e.target;
+                currentDay = dataLink.textContent;
+                exactTdForAddingANote = dataLink.parentNode;
+            }
+
+            document.getElementById('notesSection').classList.remove('hidden');
+
+            let noteHeaderID = document.getElementById('noteHeaderID');
+            let noteHeaderIDH2 = noteHeaderID.querySelector('h2');
+            noteHeaderIDH2.textContent = `Add note for ${currentDay}-${currentMonth}-${currentYear}`;
+            let noteHeaderIDDescr = noteHeaderID.querySelector('p');
+            let noteHeaderIDInput = noteHeaderID.querySelector('input');
+            noteHeaderIDDescr.textContent = '';
+            exactTdForAddingANote.querySelectorAll('.noteForTheDay').innerHTML = '';
+            if (exactTdForAddingANote.querySelectorAll('.noteForTheDay').length > 0) {
+                for (const note of exactTdForAddingANote.querySelectorAll('.noteForTheDay')) {
+                    let noteP = document.createElement('p')
+                    noteP.textContent = note.textContent;
+                    noteHeaderIDDescr.appendChild(noteP)
+                }
+            }
+
+            noteHeaderID.querySelector('button').addEventListener('click', () => {
+                if (noteHeaderIDInput.value != '') {
+                    let notificationElement = document.createElement('span')
+                    notificationElement.textContent = '-';
+                    let noteDiv = document.createElement('div')
+                    noteDiv.classList.add('hidden');
+                    noteDiv.classList.add('noteForTheDay');
+                    noteHeaderIDDescr.textContent = noteHeaderIDInput.value;
+                    noteDiv.textContent = noteHeaderIDInput.value;
+                    exactTdForAddingANote.appendChild(noteDiv);
+                    exactTdForAddingANote.appendChild(notificationElement);
+                }
+                noteHeaderIDInput.value = '';
+            })
+        })
     })
 })
