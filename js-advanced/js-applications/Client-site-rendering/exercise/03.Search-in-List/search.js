58a1796f92c5c040afce983c782f79bc4ac50d42
dilyana2001 <dilyana2001@gmail.com>
2021-08-30 23:33:09 +0300

    Search in list CSR solution

--- js-advanced/js-applications/Client-site-rendering/exercise/03.Search-in-List/search.js
+++ js-advanced/js-applications/Client-site-rendering/exercise/03.Search-in-List/search.js
@@ -1,3 +1,31 @@
-function search() {
-   // TODO
-}
+import { render } from './node_modules/lit-html/lit-html.js';
+import template from './template.js';
+import towns from './towns.js';
+
+let rootElement = document.querySelector('#towns');
+let searchBtn = document.querySelector('#searchBtn');
+let searchText = document.querySelector('#searchText');
+let resultElement = document.querySelector('#result');
+
+let townResult = template(towns);
+render(townResult, rootElement);
+
+searchBtn.addEventListener('click', () => {
+
+    let townList = Array.from(rootElement.querySelectorAll('ul li'));
+
+    townList.forEach(town => {
+        town.style.fontWeight = 'normal';
+        town.style.textDecoration = 'none';
+    })
+
+    let filtered = townList.filter(el => el.textContent.includes(searchText.value));
+
+    filtered.forEach(town => {
+        town.style.fontWeight = 'bold';
+        town.style.textDecoration = 'underline';
+    });
+
+    resultElement.textContent = `${filtered.length} matches found`;
+    searchText.value = '';
+})
