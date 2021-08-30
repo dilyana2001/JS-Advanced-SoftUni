9b8c3bd8d35eef91103d53efc893b30b7b356fc5
dilyana2001 <dilyana2001@gmail.com>
2021-08-30 15:52:07 +0300

    list town solution client side rendering

---
+++ js-advanced/js-applications/Client-site-rendering/exercise/01.List-Towns/app.js
@@ -0,0 +1,15 @@
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+
+let btnLoadTowns = document.querySelector('#btnLoadTowns');
+let inputContent = document.querySelector('#towns');
+let rootElement = document.querySelector('#root');
+
+// template
+let townTemplate = (town) => html `<li>${town}</li>`;
+
+btnLoadTowns.addEventListener('click', (e) => {
+            e.preventDefault();
+            let contentArray = inputContent.value.split(', ');
+            let ulElement = html `<ul>${contentArray.map(town=> html`${townTemplate(town)}`)}</ul>`; // template
+    render(ulElement, rootElement)
+})
