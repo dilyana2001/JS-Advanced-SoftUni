5c27fe1a367c774682ac2d9a366b450fac8e5589
dilyana2001 <dilyana2001@gmail.com>
2021-09-19 21:07:07 +0300

    Locked Profile Update

--- js-advanced/js-advanced/dom-events/dom-events-exercise/dom-events-exercise/03. Locked-Profile/app.js
+++ js-advanced/js-advanced/dom-events/dom-events-exercise/dom-events-exercise/03. Locked-Profile/app.js
@@ -1,17 +1,12 @@
 function lockedProfile() {
-    let profileButtons = document.querySelectorAll('.profile button');
-    for (let index = 0; index < profileButtons.length; index++) {
-        profileButtons[index].addEventListener('click', toggle);
-    }
-
-    function toggle(e) {
-        let button = e.target;
-        let profile = button.parentElement;
-        let showHide = profile.querySelector('div');
-        let input = profile.querySelector('input:checked');
-        if (input.value === 'unlock') {
-            showHide.style.display = showHide.style.display === 'block' ? 'none' : 'block';
-            button.textContent = button.textContent === 'Show more' ? 'Hide it' : 'Show more';
+    document.getElementById('main').addEventListener('click', (e) => {
+        if (e.target.tagName == 'BUTTON') {
+            let showHide = e.target.parentElement.querySelector('div');
+            let input = e.target.parentElement.querySelector('input:checked');
+            if (input.value == 'unlock') {
+                showHide.style.display = showHide.style.display == 'block' ? 'none' : 'block';
+                e.target.textContent = e.target.textContent == 'Show more' ? 'Hide it' : 'Show more';
+            }
         }
-    }
+    })
 }
